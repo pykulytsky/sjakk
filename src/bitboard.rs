cf77646603a593b2d321a55e7e1f64177b795654
Oleh Pykulytsky <pykulytsky@gmail.com>
2023-07-31 22:21:35 +0300

    implement bitwise operations for `u64`

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -5,13 +5,15 @@ use std::fmt::Display;
 
 /// Represents particular position on a chess board.
 /// It is used in variety of places, but the main purpose of bitboards is to represent position
-/// for
-/// pieces of each color.
+/// for pieces of each color.
+/// This struct implements all bitwise operations, so you can use it in operations with `u64`
+/// numbers.
 /// Example how to get all pawns in the position:
 /// ```no_run
 /// let white_pawns = sjakk::bitboard::Bitboard(0xFF_u64 << 8);
-/// let black_pawns = white_pawns << 40.into();
+/// let black_pawns = white_pawns << 40;
 /// let all_pawns = white_pawns | black_pawns;
+/// assert_eq!(all_pawns, 0b0000000011111111000000000000000000000000000000001111111100000000);
 /// ```
 ///
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -24,16 +26,38 @@ impl std::ops::BitOr for Bitboard {
     }
 }
 
+impl std::ops::BitOr<u64> for Bitboard {
+    type Output = Self;
+    fn bitor(self, rhs: u64) -> Self::Output {
+        Self(self.0 | rhs)
+    }
+}
+
 impl std::ops::BitAnd for Bitboard {
     type Output = Self;
     fn bitand(self, rhs: Self) -> Self::Output {
         Self(self.0 & rhs.0)
     }
 }
+
+impl std::ops::BitAnd<u64> for Bitboard {
+    type Output = Self;
+    fn bitand(self, rhs: u64) -> Self::Output {
+        Self(self.0 & rhs)
+    }
+}
+
 impl std::ops::BitXor for Bitboard {
     type Output = Self;
     fn bitxor(self, rhs: Self) -> Self::Output {
         Self(self.0 ^ rhs.0)
+    }
+}
+
+impl std::ops::BitXor<u64> for Bitboard {
+    type Output = Self;
+    fn bitxor(self, rhs: u64) -> Self::Output {
+        Self(self.0 ^ rhs)
     }
 }
 
@@ -43,15 +67,33 @@ impl std::ops::BitOrAssign for Bitboard {
     }
 }
 
+impl std::ops::BitOrAssign<u64> for Bitboard {
+    fn bitor_assign(&mut self, rhs: u64) {
+        self.0 |= rhs
+    }
+}
+
 impl std::ops::BitAndAssign for Bitboard {
     fn bitand_assign(&mut self, rhs: Self) {
         self.0 &= rhs.0
     }
 }
 
+impl std::ops::BitAndAssign<u64> for Bitboard {
+    fn bitand_assign(&mut self, rhs: u64) {
+        self.0 &= rhs
+    }
+}
+
 impl std::ops::BitXorAssign for Bitboard {
     fn bitxor_assign(&mut self, rhs: Self) {
         self.0 ^= rhs.0
+    }
+}
+
+impl std::ops::BitXorAssign<u64> for Bitboard {
+    fn bitxor_assign(&mut self, rhs: u64) {
+        self.0 ^= rhs
     }
 }
 
@@ -69,6 +111,13 @@ impl std::ops::Shl for Bitboard {
     }
 }
 
+impl std::ops::Shl<u64> for Bitboard {
+    type Output = Self;
+    fn shl(self, rhs: u64) -> Self::Output {
+        Self(self.0 << rhs)
+    }
+}
+
 impl std::ops::Shr for Bitboard {
     type Output = Self;
     fn shr(self, rhs: Self) -> Self::Output {
@@ -76,15 +125,45 @@ impl std::ops::Shr for Bitboard {
     }
 }
 
+impl std::ops::Shr<u64> for Bitboard {
+    type Output = Self;
+    fn shr(self, rhs: u64) -> Self::Output {
+        Self(self.0 >> rhs)
+    }
+}
 impl std::ops::ShlAssign for Bitboard {
     fn shl_assign(&mut self, rhs: Self) {
         self.0 <<= rhs.0
     }
 }
 
+impl std::ops::ShlAssign<u64> for Bitboard {
+    fn shl_assign(&mut self, rhs: u64) {
+        self.0 <<= rhs
+    }
+}
+
 impl std::ops::ShrAssign for Bitboard {
     fn shr_assign(&mut self, rhs: Self) {
         self.0 >>= rhs.0
+    }
+}
+
+impl std::ops::ShrAssign<u64> for Bitboard {
+    fn shr_assign(&mut self, rhs: u64) {
+        self.0 >>= rhs
+    }
+}
+
+impl PartialEq<u64> for Bitboard {
+    fn eq(&self, other: &u64) -> bool {
+        self.0.eq(other)
+    }
+}
+
+impl From<u64> for Bitboard {
+    fn from(value: u64) -> Self {
+        Self(value)
     }
 }
 
@@ -104,8 +183,9 @@ impl Display for Bitboard {
     }
 }
 
-impl From<u64> for Bitboard {
-    fn from(value: u64) -> Self {
-        Self(value)
+impl Bitboard {
+    /// Creates bitboard with `1` on the given square, with rest of the board filled with `0`.
+    pub fn from_square_number(square: u8) -> Self {
+        Self(1_u64 << square)
     }
 }
