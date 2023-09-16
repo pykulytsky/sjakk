acdb2b39f399b3b584f86f0a4484f8b4030bcfa4
Oleh Pykulytsky <pykulytsky@gmail.com>
2023-09-16 13:43:45 +0300

    remove `bin/test.rs`

--- src/bin/test.rs
+++
@@ -1,9 +0,0 @@
-use sjakk::board::Board;
-
-fn main() {
-    let board = Board::from_fen("r3k2r/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQq - 0 1").unwrap();
-    let moves = board.legal_moves();
-    for m in moves.iter() {
-        println!("{m}");
-    }
-}
